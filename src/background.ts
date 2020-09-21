8a659a4c0766f6680066980516f3ec28758b055e
dvs <dvs@crcr.ru>
2020-09-21 23:19:28 +0500

    v1.0.0

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,10 @@
+interface ImagesParts {
+    img: any
+    src: string
+    width: number
+    height: number
+}
+
 const BG = new class {
 
     constructor() {}
@@ -5,56 +12,104 @@ const BG = new class {
     /** Сделать скриншот окна */
     takeScreenshot = (windowId: number): Promise<string> => {
         return new Promise((resolve) => {
-            chrome.tabs.captureVisibleTab(windowId, {format: 'png', quality: 100}, (screenshotUrl) => {
+            chrome.tabs.captureVisibleTab(windowId, {format: 'jpeg', quality: 100}, (screenshotUrl) => {
                 resolve(screenshotUrl)
             });
+        })
+    }
+    /** Отправка сообщения о скролле и взятие скиншота */
+    moveScrollAndTakeScreenshot(tabId: number, windowId: number, params: WorkData, ypos: number, part: number): Promise<string> {
+        return new Promise((resolve) => {
+            chrome.tabs.sendMessage(tabId, {action: 'please_scroll', hash: params.hash, XY:[params.startXY[0], ypos], part}, async (response) => {
+                if (response === 'scroll_done') {
+                    window.setTimeout(async () => {
+                        let screenshotUrl = await this.takeScreenshot(windowId)
+                        resolve(screenshotUrl)
+                    }, 100)
+                }
+            })
         })
     }
     /** Добавить скрипт на страницу */
     injectScript(tab: chrome.tabs.Tab): void {
         let tabId = tab.id!
         chrome.tabs.executeScript(tabId, {file: 'contentScript.js'}, (response: any) => {
+            
             if (typeof response[0] !== 'undefined') {
                 // Если скрипт успешно загружен
                 if (response[0].status === 'script_loaded') {
-                    let hash = response[0].hash
-                    chrome.tabs.sendMessage(tabId, {action: 'give_me_dimensions', hash, tab: {tabId: tab.id, windowId: tab.windowId}})
+                    chrome.tabs.sendMessage(tabId, {action: 'give_me_dimensions', hash: response[0].hash})
                 }
             }
         })
     }
-
+    /** Начало работы над скриншотом */
+    async executeProcess(tabId: number, windowId: number, params: WorkData) {
+        let images: ImagesParts[] = []
+        let parts: [string, string][] = []
+        for (let i = 0; i < params.parts; i++) {
+            let ypos = i * params.clientHeight
+            let screenshotData = await this.moveScrollAndTakeScreenshot(tabId, windowId, params, ypos, i)
+            parts.push([screenshotData, i.toString()])
+        }
+        chrome.tabs.sendMessage(tabId, {action: 'all_back', hash: params.hash}) 
+        for (let i in parts) {
+            let img = new Image()
+            img.src = parts[i][0]
+            let imgdata: ImagesParts = await new Promise((resolve) => {
+                img.onload = function() {
+                    let that: any = this
+                    resolve({
+                        img: img,
+                        src: parts[i][0],
+                        width: that.width,
+                        height: that.height
+                    })
+                }
+            });
+            images[i] = imgdata
+        }
+        let image = this.imageProcess(images, params.scrollHeight, params.cutHeight)
+        this.downloadPart(image, '')
+    }
+    /** Обработка изображений */
+    imageProcess(images: ImagesParts[], scrollHeight: number, cutHeight: number ) {
+        let img_len = images.length - 1
+        let canvas = window.document.createElement('canvas')
+        canvas.width = images[0].width
+        canvas.height = scrollHeight
+        let context = canvas.getContext('2d')
+        let XY: number[] = [0, 0]
+        for (let i in images) {
+            let from_top = XY[1]
+            if (parseInt(i) === img_len) {
+                from_top -= cutHeight
+            }
+            context!.drawImage(images[i].img, XY[0], from_top, images[i].width, images[i].height)
+            XY[1] += images[i].height
+        }
+        return canvas.toDataURL('image/jpeg')
+    }
+    /** скачивание изображения по url */
+    downloadPart(screenshotUrl: string, prefix: string) {
+        let ins = document.createElement('a')
+            ins.setAttribute('download', (prefix?prefix+'_':'')+'screenshot.jpg')
+            ins.href = screenshotUrl
+        let ndiv = document.createElement('div')
+            ndiv.appendChild(ins)
+        document.body.innerHTML = ndiv.innerHTML
+        document.getElementsByTagName('a')[0].click()
+        document.body.removeChild(document.getElementsByTagName('a')[0])
+        return true
+    }
 }
-
 // Отработка нажатия на иконку в быстром запуске
-chrome.browserAction.onClicked.addListener((tab) => BG.injectScript(tab));
-
+chrome.browserAction.onClicked.addListener((tab) => BG.injectScript(tab))
 // Отработка получения сообщений из контент-скриптов
-// chrome.runtime.onMessage.addListener((msg, sender, res) => {
-//     if (msg.action === 'script_loaded') {
-//         let tabId: number = sender.tab?.id!
-//         // scroll to 0 (zero)
-//         // make screenshot
-//         console.log(msg);
-//         chrome.tabs.sendMessage(tabId, {action: 'fuck you!'})
-//     }
-    
-// })
-
-// chrome.tabs.captureVisibleTab(null!, {format: 'jpeg', quality: 100}, function(screenshotUrl) {
-//     let scrollHeight = Math.max(
-//         document.body.scrollHeight, document.documentElement.scrollHeight,
-//         document.body.offsetHeight, document.documentElement.offsetHeight,
-//         document.body.clientHeight, document.documentElement.clientHeight
-//       );
-
-//     var ins = document.createElement('a');
-//         ins.setAttribute('download', 'screenshot.jpg');
-//         ins.href = screenshotUrl;
-//     var ndiv = document.createElement('div');
-//         ndiv.appendChild(ins);
-        
-//     document.body.innerHTML = ndiv.innerHTML
-//     document.getElementsByTagName("a")[0].click();
-//     document.body.removeChild(document.getElementsByTagName("a")[0]);
-// });
+chrome.runtime.onMessage.addListener((msg, sender, res) => {
+    if (msg.action === 'make_some_work') {
+        let tabId: number = sender.tab?.id!
+        let windowId: number = sender.tab?.windowId!
+        BG.executeProcess(tabId, windowId, msg.params)
+    }
+})
--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,19 +1,22 @@
-// chrome.runtime.sendMessage(chrome.runtime.id, {action:'script_loaded'})
-//     chrome.runtime.onMessage.addListener((msg) => {
-//         console.log(msg);
-//     })
-
 interface WorkData {
     parts: number
     clientHeight: number  
     cutHeight: number
     startXY: number[],
-    hash: string
+    hash: string,
+    scrollHeight: number
 }
-
 (function(){
-    // Суперуникальный идентификатор скрипта
     let hash = window.btoa((new Date).getTime()+'sortofhash')
+    let bodyOverflow = ''
+    let baseStartY = 0
+    
+    function createOpacityStyle() {
+        let style = document.createElement('style');
+        document.head.appendChild(style);
+        style?.sheet?.insertRule('.nullopacityelement {opacity: 0}');
+        return true
+    }
     function getWorkParams(startX: number, startY: number, hash: string): WorkData {
         let scrollHeight = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight,document.body.offsetHeight, document.documentElement.offsetHeight,document.body.clientHeight, document.documentElement.clientHeight)
         let clientHeight = document.documentElement.clientHeight
@@ -23,13 +26,31 @@ interface WorkData {
         if (modul !== 0 && parts !== 1) {
             cutHeight = clientHeight - modul
         }
+        baseStartY = startY
         return {
             parts,
             clientHeight,
             cutHeight,
             startXY: [startX, startY],
+            scrollHeight,
             hash
         }
+    }
+    function hideFixedElements() {
+        let els = document.body.getElementsByTagName('*');
+        for (let i=0;i<els.length;i++) {
+            if (window.getComputedStyle(els[i],null).getPropertyValue('position') == 'fixed') {
+                els[i].classList.add('nullopacityelement')
+            }
+        }
+        return true
+    }
+    function showFixedElements() {
+        let els = document.body.getElementsByClassName('nullopacityelement')
+        for (let i=0;i<els.length;i++) {
+            els[i].classList.remove('nullopacityelement')
+        }
+        return true
     }
     function process(params: WorkData): void {
         chrome.runtime.sendMessage(chrome.runtime.id, {
@@ -37,15 +58,32 @@ interface WorkData {
             params
         })
     }
-    chrome.runtime.onMessage.addListener((msg) => {
+    function allBack() {
+        showFixedElements()
+        document.body.style.overflow = bodyOverflow
+        window.scrollTo({
+            top: baseStartY,
+            behavior: "auto"
+        })
+    }
+    chrome.runtime.onMessage.addListener((msg, sender, response) => {
         if (msg.hash === hash) {
-            if (msg.action === 'give_me_dimensions') {
-                process(getWorkParams(window.pageXOffset, window.pageYOffset, hash))
-            }
+            if (msg.action === 'all_back') allBack()
+            if (msg.action === 'give_me_dimensions') process(getWorkParams(window.pageXOffset, window.pageYOffset, hash))
             if (msg.action === 'please_scroll') {
-                window.scrollTo(...msg.XY)
+                if (msg.part === 1) {
+                    hideFixedElements()
+                }
+                window.scrollTo({
+                    top: msg.XY[1],
+                    behavior: "auto"
+                })
+                response('scroll_done')
             }
         }
     });
+    bodyOverflow = document.body.style.overflow
+    createOpacityStyle()
+    document.body.style.overflow = 'hidden'
     return {status: 'script_loaded', hash}
 })()
